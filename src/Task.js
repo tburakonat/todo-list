e799c1301b7333cdd819f469c586648facc7ad75
tburakonat <tburakonat@gmail.com>
2022-08-09 17:09:06 +0200

    created method to mark task as done

--- src/Task.js
+++ src/Task.js
@@ -6,6 +6,7 @@ export class Task {
 		this.description = description
 		this.dueDate = dueDate
 		this.priority = priority
+		this.isDone = false
 		this.id = uuidv4()
 	}
 
@@ -18,23 +19,50 @@ export class Task {
 		const priority = document.createElement('p')
 
 		// TODO: Create buttons to delete task and to mark as done
+		const buttonsDiv = document.createElement('div')
+		const doneButton = document.createElement('button')
+		const removeButton = document.createElement('button')
 
 		title.textContent = this.title
 		description.textContent = this.description
 		dueDate.textContent = this.dueDate
 		priority.textContent = this.priority
+		doneButton.textContent = 'Mark as done'
+		removeButton.textContent = 'Remove Task'
 
 		task.classList.add('task-card')
 		title.classList.add('task-title')
 		description.classList.add('task-description')
 		dueDate.classList.add('task-due-date')
 		priority.classList.add('task-priority')
+		buttonsDiv.classList.add('task-buttons')
+		doneButton.classList.add('done-button', 'not-done')
+		removeButton.classList.add('remove-button')
+
+		doneButton.onclick = this.changeStatus
 
 		task.appendChild(title)
 		task.appendChild(description)
 		task.appendChild(dueDate)
 		task.appendChild(priority)
+		buttonsDiv.appendChild(doneButton)
+		buttonsDiv.appendChild(removeButton)
+		task.appendChild(buttonsDiv)
 
 		tasksDiv.appendChild(task)
+	}
+
+	changeStatus = e => {
+		this.isDone = !this.isDone
+		const button = e.target
+		if (this.isDone) {
+			button.textContent = 'Mark as not done'
+			button.classList.add('done')
+			button.classList.remove('not-done')
+		} else {
+			button.textContent = 'Mark as done'
+			button.classList.add('not-done')
+			button.classList.remove('done')
+		}
 	}
 }
