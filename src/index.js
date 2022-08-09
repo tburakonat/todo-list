d265496affc5f7d16c5a59e9dae94e0f389738fc
tburakonat <tburakonat@gmail.com>
2022-08-09 14:44:35 +0200

    added Task class and removed dist from repo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+import { Task } from './Task'
+
 const contentDiv = document.getElementById('content')
 const h1 = document.createElement('h1')
 h1.textContent = 'Hello World'
 contentDiv.appendChild(h1)
+
+const myTask = new Task('Einkaufen', 'Fleisch, Fisch, Eier', '12.08.2022', 'hoch')
+console.log(myTask)
